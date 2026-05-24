4e67827a89c519c1f71264d33d4d88404c322974
megashmepe-glitch <megashmepe@gmail.com>
2026-05-24 14:42:20 +0200

    Remove URL from main.py

--- main.py
+++ main.py
@@ -251,3 +251,4 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+https://web-production-462b4.up.railway.app/
